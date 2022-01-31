9c7dd5491ba0077944949d3c759f77715e6840e6
unknown <you@example.com>
2022-01-31 19:51:13 -0300

    Implementação do hamb. menu no mobile

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,14 +1,36 @@
+import { useState } from 'react';
+
 import InsureLogo from '../../public/logo.svg';
 import BurgerIcon from '../../public/icon-hamburger.svg';
+import CloseIcon from '../../public/icon-close.svg';
+
+import styles from './style.module.scss';
 
 export default function NavBar() {
-    return (
-        <div>
-            <img src={ InsureLogo } alt="Insure" />
+    const [openNav, setOpenNav] = useState(false);
 
-            <button>
-                <img src={ BurgerIcon } alt="" />
-            </button>
-        </div>
+    const handleOpenNav = () => {
+        setOpenNav(!openNav);
+    };
+
+    return (
+        <>
+            <section className={styles.containerNav}>
+                <img src={ InsureLogo } alt="Insure" />
+
+                <button onClick={handleOpenNav}>
+                    { openNav ? <img src={ CloseIcon } alt="" /> : <img src={ BurgerIcon } alt="" /> }
+                </button>
+            </section>
+
+            <nav className={ openNav ? `${styles.menuMobile} ${styles.showMenu}` : `${styles.menuMobile}` }>
+                <ul>
+                    <li>How we work</li>
+                    <li>Blog</li>
+                    <li>Account</li>
+                    <li id={ styles.viewPlans }>View Plans</li>
+                </ul>
+            </nav>
+        </>
     );
 };
